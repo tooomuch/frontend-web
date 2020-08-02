30d0792a1a5a9d6bd92757ae9f7e781395fbe0e1
Danuel <public.danuel@gmail.com>
2020-08-03 00:24:59 +0900

    add some routes

--- src/App/Routes/index.tsx
+++ src/App/Routes/index.tsx
@@ -1,12 +1,33 @@
+import { CommunityCategory } from "App/Pages/CommunityCategory";
+import { CommunityCategoryList } from "App/Pages/CommunityCategoryList";
+import { CommunityHome } from "App/Pages/CommunityHome";
+import { CommunityPost } from "App/Pages/CommunityPost";
 import { Home } from "App/Pages/Home";
 import { NotFound } from "App/Pages/NotFound";
-import { HOME, NOT_FOUND } from "constants/routes";
+import { PoliciesPrivacyPolicy } from "App/Pages/PoliciesPrivacyPolicy";
+import { PoliciesTermsOfService } from "App/Pages/PoliciesTermsOfService";
+import {
+  COMMUNITY_CATEGORY,
+  COMMUNITY_CATEGORY_LIST,
+  COMMUNITY_HOME,
+  COMMUNITY_POST,
+  HOME,
+  NOT_FOUND,
+  POLICIES_PRIVACY_POLICY,
+  POLICIES_TERMS_OF_SERVICE
+} from "constants/routes";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router";
 
 export const Routes = () => (
   <Switch>
     <Route path={HOME} component={Home} exact />
+    <Route path={COMMUNITY_HOME} component={CommunityHome} exact />
+    <Route path={COMMUNITY_CATEGORY_LIST} component={CommunityCategoryList} exact />
+    <Route path={COMMUNITY_CATEGORY} component={CommunityCategory} exact />
+    <Route path={COMMUNITY_POST} component={CommunityPost} exact />
+    <Route path={POLICIES_TERMS_OF_SERVICE} component={PoliciesTermsOfService} exact />
+    <Route path={POLICIES_PRIVACY_POLICY} component={PoliciesPrivacyPolicy} exact />
     <Route path={NOT_FOUND} component={NotFound} />
     <Redirect to={NOT_FOUND} />
   </Switch>
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,2 +1,8 @@
 export const HOME = "/";
 export const NOT_FOUND = "/notfound";
+export const COMMUNITY_HOME = "/s/:twitchId";
+export const COMMUNITY_CATEGORY_LIST = "/s/:twitchId/c";
+export const COMMUNITY_CATEGORY = "/s/:twitchId/c/:categoryId";
+export const COMMUNITY_POST = "/s/:twitchId/p/:postId";
+export const POLICIES_TERMS_OF_SERVICE = "/policy/terms-of-service";
+export const POLICIES_PRIVACY_POLICY = "/policy/privacy-policy";
