abb95da6a083990c76d0542aedfa0f9184b3ef5c
Danuel <public.danuel@gmail.com>
2020-08-04 01:14:50 +0900

    add style in header

--- src/App/Templates/LayoutTemplate/Header/index.tsx
+++ src/App/Templates/LayoutTemplate/Header/index.tsx
@@ -1,3 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-export const Header = () => <div>Header</div>;
+const Layout = styled.div`
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+
+  border-radius: 2px;
+  border: 1px solid hsl(0 0% 84%);
+  border-top-width: 0.25rem;
+  box-shadow: 4px 4px 1px 0 hsl(0 0% 84%);
+  background-color: white;
+`;
+
+export const Header = () => <Layout>Header</Layout>;
